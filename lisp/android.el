22763248a775468f34385c2fbddedf8a561c618a
Lorenzo Masini <l.masini@cantierecreativo.net>
2014-11-24 01:45:51 +0100

    Removed unused android configuration.

--- lisp/android.el
+++
@@ -1,6 +0,0 @@
-;; Android SDK mode
-(vendor 'android-mode)
-
-(setq android-mode-sdk-dir '/opt/android-sdk-linux)
-(if (getenv "ANDROID_SDK")
-    (setq android-mode-sdk-dir (getenv "ANDROID_SDK")) )
