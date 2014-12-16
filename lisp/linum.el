5b78d62d5f6ca8fc596636b976c0feed4651563a
Lorenzo Masini <l.masini@cantierecreativo.net>
2014-12-16 17:45:58 +0100

    Added calendar to linum-disabled-modes.

--- lisp/linum.el
+++ lisp/linum.el
@@ -2,7 +2,7 @@
 (setq linum-format "%3d ")
 (global-linum-mode)
 
-(setq linum-disabled-modes '("Info" "dired" "eshell" "magit-status"))
+(setq linum-disabled-modes '("Info" "dired" "eshell" "magit-status" "calendar"))
 
 (dolist (mode linum-disabled-modes)
   (add-hook (intern (concat mode "-mode-hook")) (lambda () (linum-mode 0))))
